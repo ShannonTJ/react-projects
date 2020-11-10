457e92cb0f33ba11fa072fe0662b1ff343618464
Shannon TJ <shannontj@shaw.ca>
2020-11-09 18:25:43 -0700

    first commit

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,27 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+const [people, setPeople] = useState(data);
+
+  function displayText(length) {
+      if(length === 1) {
+        return <h3>{length} birthday today</h3>;
+      }
+      else {
+        return <h3>{length} birthdays today</h3>;
+      }    
+  }
+
+  return (
+  <main>
+    <section className='container'>
+      {displayText(people.length)}
+      <List people={people} setPeople={setPeople}></List>
+      <button className='btn' onClick={() => setPeople([])}>Clear All</button>
+    </section>
+    </main>
+    );
 }
 
 export default App;
--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people, setPeople}) => {
+
+  function removePerson(id) {
+    setPeople(people.filter((p) => p.id !== id));
+  }
+
   return (
     <>
-      <h2>list component</h2>
+     {people.map((p) => {
+       const {id, name, age, image} = p;
+       return <article key={id} className='person'> 
+       <img src={image} alt={name} /> 
+
+       <div className='outer'> 
+         <h4 className='inner'>{name}</h4> 
+         <button className='inner' onClick={() => removePerson(p.id)}>Remove</button>
+         <p>{age} years</p>
+        </div>
+
+     </article> 
+    })}
     </>
   );
 };

4d25180db2a3a85f0f15e716610a18a65ab8ea79
Shannon TJ <shannontj@shaw.ca>
2020-11-10 15:30:39 -0700

    tours project

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -5,7 +5,55 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    setTours(tours.filter((t) => t.id !== id));
+  }
+
+  const fetchTours = async () => {
+
+    try{
+    const response = await fetch(url);
+    const tours = await response.json();
+    setLoading(false);
+    setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if(loading) {
+    return (
+    <main>
+      <Loading></Loading>
+    </main>
+    );
+  }
+
+  if(tours.length === 0) {
+    return ( 
+    <main>
+      <div className='title'>
+        <h2>No Tours Left</h2>
+        <button className='btn' onClick={fetchTours}>Refresh</button>
+      </div>
+    </main>
+    )
+  }
+
+  return (
+  <main>
+    <Tours tours={tours} removeTour={removeTour}></Tours>
+  </main>
+  )
 }
 
 export default App
--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = ({tours, removeTour}) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>Our Tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((t) => {
+          return <Tour key={t.id} {...t} removeTour={removeTour}></Tour>
+        }
+        )}
+      </div>
+    </section>
+  )
 };
 
 export default Tours;

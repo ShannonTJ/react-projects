91085f541d68d87051f414e52f458d82d884ff66
Shannon TJ <shannontj@shaw.ca>
2020-11-13 23:22:42 -0700

    menu setup

--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+
+  return (
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const {id, title, img, desc} = menuItem;
+        return <article key={id} className='menu-item'>
+          <img src={img} alt={title} className='photo'/>
+        </article>
+       })}
+    </div>
+  )
 };
 
 export default Menu;

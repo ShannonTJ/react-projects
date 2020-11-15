6dfa7fe4b93b75354c84738ae7f6d3344481d250
Shannon TJ <shannontj@shaw.ca>
2020-11-14 17:12:50 -0700

    menu project

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -3,10 +3,25 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+//get only unique categories from the data
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
 
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+
+    if(category === 'all')
+      {
+        setMenuItems(items);
+        return;
+      }
+
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems);
+  }
 
   return (
     <>
@@ -16,7 +31,7 @@ function App() {
       <h2>Our Menu</h2>
       <div className='underline'></div>
       </div>
-      <Categories/>
+      <Categories categories={categories} filterItems={filterItems}/>
       <Menu items={menuItems}/>
     </section>
   </main>
